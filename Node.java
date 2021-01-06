79ada5504aacb9560b8a441950f529212a3f8f10
OnikuSpedu <srakibnyc1@gmail.com>
2021-01-06 13:23:33 -0500

    Added constructor and get and set methods for Node.java.

--- Node.java
+++ Node.java
@@ -1,6 +1,28 @@
 public class Node{
-    public Node(String value){/*create a constructor*/}
+
     private String data;
     private Node next,prev;
-    //write get/set methods for all three instance variables.
+    
+    public Node(String value){
+        data = value;
+    }
+    
+    public String getData() {
+        return this.data;
+    }
+    public Node getNext() {
+        return this.next;
+    }
+    public Node getPrev() {
+        return this.prev;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+    public void setNext(Node n) {
+        this.next = n;
+    }
+    public void setPrev(Node n) {
+        this.prev = n;
+    }
 }
